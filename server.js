984b7c31b1499d4d95a338cab8b0f4f3788ffd28
apleek3 <apleek3@gmail.com>
2018-09-25 14:31:11 -0700

    JAWSDB patch

--- server.js
+++ server.js
@@ -34,7 +34,8 @@ require("./routes/game-routes")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+var PORT = process.env.PORT || 8080;
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
