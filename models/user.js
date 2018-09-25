103c1ab1d1313af5624dd443aab3ec9e6e94c819
apleek3 <apleek3@gmail.com>
2018-09-24 18:36:48 -0700

    1st page connected with Kathrin's

--- models/user.js
+++ models/user.js
@@ -1,10 +1,21 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define("User", {
     // Giving the User model a name of type STRING
     name: DataTypes.STRING
+    ,
+    email: {
+      type: DataTypes.STRING,
+      isEmail: true,
+      isUnique: true,
+      allowNull: false
+    }
+    // ,
+    // pswd: {
+
+    // }
   });
 
-  User.associate = function(models) {
+  User.associate = function (models) {
     // Associating User with Posts
     // When an User is deleted, also delete any associated Posts
     User.hasMany(models.Game, {
